54784febc805e11de310cae4154233e7abcf46f9
VilmerJ <vilmer@kopet.se>
2023-03-27 22:51:32 +0200

    SL API now using coordinates instead of stop id

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { getTrafficInfo } from './tripSource'
+import { getTrafficInfo, CoordsObj } from './tripSource'
 
-getTrafficInfo('740000001', '740000003'); 
+const coords: CoordsObj = {
+  originCoordLat: '59.34967',
+  originCoordLong: '18.00375',
+  destCoordLat: '59.34609',
+  destCoordLong: '18.07181'
+}
+
+getTrafficInfo(coords); 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/tripSource.tsx
+++ src/tripSource.tsx
@@ -1,17 +1,32 @@
 import axios from 'axios'
 import { trafikLabKey } from './keys'
 
-function getTrafficInfo(origin: string, destination: string) {
-  const apiUrl = `https://api.resrobot.se/v2.1/trip?format=json&originId=${origin}&destId=${destination}&passlist=true&showPassingPoints=true&accessId=${trafikLabKey}`
+// Structure on parameter object in getTrafficInfo.
+interface CoordsObj {
+  originCoordLat: string
+  originCoordLong: string
+  destCoordLat: string
+  destCoordLong: string
+}
+
+function getTrafficInfo(coordsObj: CoordsObj) {
+  let apiUrl = `https://api.resrobot.se/v2.1/trip?format=json&accessId=${trafikLabKey}&showPassingPoints=true&`
+
+  const params = new URLSearchParams() // uses URLSearchParams to build URL with the given information
+  for (const key in coordsObj) {
+    params.append(key, coordsObj[key as keyof CoordsObj])
+  }
+  apiUrl += params.toString()
 
   axios
     .get(apiUrl)
     .then((response) => {
-      console.log(response.data)
+      console.log(response.data) // just logging response to terminal for now
     })
     .catch((error) => {
       console.error(error)
     })
 }
 
+export type { CoordsObj }
 export { getTrafficInfo }
