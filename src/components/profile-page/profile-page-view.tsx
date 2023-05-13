01a0f946c86cd80271e2d8018fe9f1a452fd6150
Olle Jernström WSL <ollejer@ug.kth.se>
2023-05-13 11:15:56 +0200

    hide overflow x and build base form outline component

--- src/components/profile-page/profile-page-view.tsx
+++ src/components/profile-page/profile-page-view.tsx
@@ -1,11 +1,24 @@
 
 
 
+import { Box, Container, Paper, Stack, Autocomplete, Loader, Checkbox, Button, Alert } from '@mantine/core'
+import { DateInput, TimeInput } from '@mantine/dates'
+import { IconAlertCircle } from '@tabler/icons-react'
 import React from 'react'
+import UseCalButton from '../calendar-buttons/use-cal-button'
 
 const ProfilePageView = () => {
   return (
-    <div>Profile-page-presenter</div>
+    <Box w='100vw'>
+      <Container>
+        <Paper m='xl' p='xl' withBorder>
+          <Stack spacing='xs'>
+            hello
+          </Stack>
+        </Paper>
+      </Container>
+    </Box>
+
   )
 }
 
