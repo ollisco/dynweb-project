1333ac12ce217590674eb3294b79cfc169173961
Oscar Cusin <osse30994@gmail.com>
2023-06-09 17:53:26 +0200

    Tweaks.

--- src/auth.ts
+++ src/auth.ts
@@ -24,15 +24,16 @@ class Auth {
 
   constructor(model: Model) {
     makeAutoObservable(this)
-    this.model = model
-    const savedUser = localStorage.getItem('user')
-    this.user = savedUser ? (JSON.parse(savedUser) as UserCredential) : null
-    if (this.user) this.loadData()
     this.signIn = this.signIn.bind(this)
     this.signOut = this.signOut.bind(this)
     this.setUser = this.setUser.bind(this)
     this.loadData = this.loadData.bind(this)
     this.saveData = this.saveData.bind(this)
+    this.model = model
+
+    const savedUser = localStorage.getItem('user')
+    this.user = savedUser ? (JSON.parse(savedUser) as UserCredential) : null
+    if (this.user) this.loadData()
     observe(this.model, (change) => {
       if (change.name === 'savedHomeAddress' || change.name === 'routines') {
         this.saveData()
@@ -59,8 +60,7 @@ class Auth {
 
   async loadData() {
     if (this.user) {
-      const doc = await db.collection('users').doc(this.user.user.uid).get()
-      const data = doc.data()
+      const data = (await db.collection('users').doc(this.user.user.uid).get()).data()
       if (data) {
         if (data.homeAddress) {
           this.model.setHomeAddress(data.homeAddress)
@@ -74,9 +74,9 @@ class Auth {
   }
 
   saveData() {
-    const homeAddress = this.model.homeAddress ? this.model.homeAddress : ''
-    const routines = this.model.routines ? this.model.routines : []
     if (this.user) {
+      const homeAddress = this.model.homeAddress ? this.model.homeAddress : ''
+      const routines = this.model.routines ? this.model.routines : []
       db.collection('users').doc(this.user.user.uid).set({
         homeAddress: homeAddress,
         routines: routines,
